fd25911e18313d0f3a49b00f6502835bc6ceafbe
markuczy <129275100+markuczy@users.noreply.github.com>
2024-07-09 06:50:53 +0200

    feat: welcome-ui as webcomponent (#41)
    * feat: module exposed
    
    * feat: libs upgrade
    
    * fix: output uniqueName

--- src/app/onecx-welcome-remote.module.ts
+++ src/app/onecx-welcome-remote.module.ts
@@ -1,27 +1,45 @@
-import { HttpClient } from '@angular/common/http'
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { APP_INITIALIZER, DoBootstrap, Injector, NgModule } from '@angular/core'
+import { createCustomElement } from '@angular/elements'
+import { Router, RouterModule, Routes } from '@angular/router'
 import { MissingTranslationHandler, TranslateLoader, TranslateModule } from '@ngx-translate/core'
+import { initializeRouter, startsWith } from '@onecx/angular-webcomponents'
 
 import {
   addInitializeModuleGuard,
   AppStateService,
   ConfigurationService,
   createTranslateLoader,
+  PortalApiConfiguration,
   PortalCoreModule,
   PortalMissingTranslationHandler
 } from '@onecx/portal-integration-angular'
+import { AngularAuthModule } from '@onecx/angular-auth'
+import { AppEntrypointComponent } from './app-entrypoint.component'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { Configuration } from './shared/generated'
+import { environment } from 'src/environments/environment'
+
+function apiConfigProvider(configService: ConfigurationService, appStateService: AppStateService) {
+  return new PortalApiConfiguration(Configuration, environment.apiPrefix, configService, appStateService)
+}
 
 const routes: Routes = [
   {
-    path: '',
+    matcher: startsWith(''),
     loadChildren: () => import('./welcome/welcome.module').then((m) => m.WelcomeModule)
   }
 ]
 @NgModule({
+  declarations: [AppEntrypointComponent],
   imports: [
+    HttpClientModule,
+    AngularAuthModule,
+    BrowserModule,
+    BrowserAnimationsModule,
     PortalCoreModule.forMicroFrontend(),
-    RouterModule.forChild(addInitializeModuleGuard(routes)),
+    RouterModule.forRoot(addInitializeModuleGuard(routes)),
     TranslateModule.forRoot({
       isolate: true,
       loader: {
@@ -33,11 +51,27 @@ const routes: Routes = [
     })
   ],
   exports: [],
-  providers: [ConfigurationService],
+  providers: [
+    ConfigurationService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeRouter,
+      multi: true,
+      deps: [Router, AppStateService]
+    },
+    { provide: Configuration, useFactory: apiConfigProvider, deps: [ConfigurationService, AppStateService] }
+  ],
   schemas: []
 })
-export class OneCXWelcomeModule {
-  constructor() {
+export class OneCXWelcomeModule implements DoBootstrap {
+  constructor(private injector: Injector) {
     console.info('OneCX Welcome Module constructor')
+  }
+
+  ngDoBootstrap(): void {
+    const appEntrypoint = createCustomElement(AppEntrypointComponent, {
+      injector: this.injector
+    })
+    customElements.define('ocx-welcome-component', appEntrypoint)
   }
 }
