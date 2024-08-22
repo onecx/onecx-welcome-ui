a871507a4287338c2523fb3db5453c4ea10de567
Christian Badura <christian.badura@capgemini.com>
2024-08-22 11:19:23 +0200

    feat: detail tests

--- src/app/welcome/welcome-configure/image-detail/image-detail.component.spec.ts
+++ src/app/welcome/welcome-configure/image-detail/image-detail.component.spec.ts
@@ -1,50 +1,23 @@
 import { HttpClient } from '@angular/common/http'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { NO_ERRORS_SCHEMA } from '@angular/core'
+import { OverlayPanelModule } from 'primeng/overlaypanel'
+import { ButtonModule } from 'primeng/button'
+import { By } from '@angular/platform-browser'
 import { ComponentFixture, waitForAsync, TestBed } from '@angular/core/testing'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
-import {
-  PortalMessageService,
-  createTranslateLoader,
-  AppStateService,
-  UserService
-} from '@onecx/portal-integration-angular'
 import { DialogModule } from 'primeng/dialog'
 import { InputSwitchModule } from 'primeng/inputswitch'
-import { of } from 'rxjs'
-import { ImagesInternalAPIService } from 'src/app/shared/generated'
+import { createTranslateLoader, AppStateService } from '@onecx/portal-integration-angular'
+
 import { ImageDetailComponent } from './image-detail.component'
-import { OverlayPanelModule } from 'primeng/overlaypanel'
-import { ButtonModule } from 'primeng/button'
+import { ImageInfo } from 'src/app/shared/generated'
 
 describe('ImageDetailComponent', () => {
   let component: ImageDetailComponent
   let fixture: ComponentFixture<ImageDetailComponent>
-
-  const msgServiceSpy = jasmine.createSpyObj<PortalMessageService>('PortalMessageService', [
-    'success',
-    'error',
-    'info',
-    'warning'
-  ])
-
-  const apiServiceSpy = {
-    createImageInfo: jasmine.createSpy('createImageInfo').and.returnValue(of({})),
-    createImage: jasmine.createSpy('createImage').and.returnValue(of({})),
-    updateImageInfo: jasmine.createSpy('updateImageInfo').and.returnValue(of({}))
-  }
-
-  const mockUserService = {
-    lang$: {
-      getValue: jasmine.createSpy('getValue')
-    },
-    profile$: {
-      getValue: jasmine.createSpy('getValue'),
-      asObservable: jasmine.createSpy('asObservable')
-    }
-  }
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -66,21 +39,8 @@ describe('ImageDetailComponent', () => {
         ReactiveFormsModule,
         ButtonModule
       ],
-      schemas: [NO_ERRORS_SCHEMA],
-      providers: [
-        { provide: PortalMessageService, useValue: msgServiceSpy },
-        { provide: ImagesInternalAPIService, useValue: apiServiceSpy },
-        { provide: UserService, useValue: mockUserService }
-      ]
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents()
-    msgServiceSpy.success.calls.reset()
-    msgServiceSpy.error.calls.reset()
-    msgServiceSpy.info.calls.reset()
-    msgServiceSpy.warning.calls.reset()
-    apiServiceSpy.createImage.calls.reset()
-    apiServiceSpy.createImageInfo.calls.reset()
-    apiServiceSpy.updateImageInfo.calls.reset()
-    mockUserService.lang$.getValue.and.returnValue('de')
   }))
 
   beforeEach(() => {
@@ -89,7 +49,58 @@ describe('ImageDetailComponent', () => {
     component.displayDetailDialog = true
     fixture.detectChanges()
   })
+
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should close dialog', () => {
+    fixture.detectChanges()
+
+    const dElement = fixture.debugElement
+
+    const dialog = dElement.nativeElement.querySelector('p-dialog')
+    expect(dialog.attributes.getNamedItem('ng-reflect-visible').value).toBeTruthy()
+    const closeButton = dElement.queryAll(By.css('button'))[1]
+    expect(closeButton).toBeTruthy()
+    let dialogCloseEvent
+    component.hideDialogAndChanged.subscribe((event: any) => {
+      dialogCloseEvent = event
+    })
+    expect(dialogCloseEvent).toBeUndefined()
+    closeButton.nativeElement.click()
+
+    expect(dialogCloseEvent).toBeDefined()
+    if (dialogCloseEvent) {
+      expect(dialogCloseEvent).toBe(false)
+    }
+  })
+
+  describe('buildImageSrc', () => {
+    it('should return the base64 image source if the image is found', () => {
+      const imageInfo = { imageId: '1', url: 'http://example.com/image1.png' } as ImageInfo
+      const result = component.buildImageSrc(imageInfo)
+      expect(result).toBe('http://example.com/image1.png')
+    })
+
+    it('should return the image URL if the image is not found', () => {
+      const imageInfo = { imageId: '3', url: 'http://example.com/image3.png' } as ImageInfo
+      const result = component.buildImageSrc(imageInfo)
+      expect(result).toBe('http://example.com/image3.png')
+    })
+
+    it('should return the correct URL if images array is empty', () => {
+      component.images = []
+      const imageInfo = { imageId: '1', url: 'http://example.com/image1.png' } as ImageInfo
+      const result = component.buildImageSrc(imageInfo)
+      expect(result).toBe('http://example.com/image1.png')
+    })
+
+    it('should return the correct URL if imageData is empty', () => {
+      component.images = [{ imageId: '1', mimeType: 'image/png' }]
+      const imageInfo = { imageId: '1', url: 'http://example.com/image1.png' } as ImageInfo
+      const result = component.buildImageSrc(imageInfo)
+      expect(result).toBe('data:image/png;base64,undefined')
+    })
   })
 })
