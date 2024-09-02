2f8b200dc11f633b09c6805e075ebfa62139330a
Christian Badura <christian.badura@capgemini.com>
2024-09-02 14:41:03 +0200

    fix: finish configure tests

--- src/app/welcome/welcome-configure/welcome-configure.component.spec.ts
+++ src/app/welcome/welcome-configure/welcome-configure.component.spec.ts
@@ -5,13 +5,14 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
 import { AppStateService, createTranslateLoader, PortalMessageService } from '@onecx/portal-integration-angular'
 import { of, throwError } from 'rxjs'
-import { ImageInfo, ImagesInternalAPIService } from 'src/app/shared/generated'
+import { ImageDataResponse, ImageInfo, ImagesInternalAPIService } from 'src/app/shared/generated'
 import { WelcomeConfigureComponent } from './welcome-configure.component'
 import { CardModule } from 'primeng/card'
 import { ButtonModule } from 'primeng/button'
 import { ImageCreateComponent } from './image-create/image-create.component'
 import { DialogModule } from 'primeng/dialog'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { Workspace } from '@onecx/integration-interface'
 
 const imageData: ImageInfo[] = [
   {
@@ -27,6 +28,13 @@ const imageData: ImageInfo[] = [
   { id: '123456', imageId: '123456', visible: true, position: '3', workspaceName: 'ws' },
   { id: '1234567', imageId: '1234567', visible: true, position: '3', workspaceName: 'ws' }
 ]
+
+const ws: Workspace = {
+  workspaceName: 'wsName',
+  portalName: 'wsName',
+  baseUrl: 'url',
+  microfrontendRegistrations: []
+}
 
 fdescribe('WelcomeConfigureComponent', () => {
   let component: WelcomeConfigureComponent
@@ -91,24 +99,66 @@ fdescribe('WelcomeConfigureComponent', () => {
     expect(component).toBeTruthy()
   })
 
-  xit('should handle error when fetching imageinfos', () => {
-    apiServiceSpy.getAllImageInfosByWorkspaceName.and.returnValue(throwError(() => new Error()))
+  describe('fetchImageData', () => {
+    beforeEach(() => {
+      component.workspace = ws
+    })
 
-    component.fetchImageInfos()
+    it('should get infos for all images', (done) => {
+      apiServiceSpy.getAllImageInfosByWorkspaceName.and.returnValue(of(imageData))
 
-    expect(msgServiceSpy.error).toHaveBeenCalledWith({ summaryKey: 'GENERAL.IMAGES.NOT_FOUND' })
+      component.fetchImageInfos()
+
+      component.imageData$?.subscribe({
+        next: (images) => {
+          expect(images.length).toBe(5)
+          done()
+        },
+        error: done.fail
+      })
+    })
+
+    it('should handle error when fetching imageinfos', (done) => {
+      const err = { status: 404 }
+      apiServiceSpy.getAllImageInfosByWorkspaceName.and.returnValue(throwError(() => err))
+      spyOn(console, 'error')
+      component.imageData = imageData
+
+      component.fetchImageInfos()
+
+      component.imageData$?.subscribe({
+        next: () => {
+          expect(console.error).toHaveBeenCalledWith('getAllImageInfosByWorkspaceName():', err)
+          done()
+        },
+        error: done.fail
+      })
+    })
   })
 
-  it('should handle error when fetching imageData', () => {
-    apiServiceSpy.getImageById.and.returnValue(throwError(() => new Error()))
-    component.imageData = [{ id: '123', imageId: '123', visible: true, position: '1', workspaceName: 'w1' }]
-    component.fetchImageData()
+  describe('fetchImageData', () => {
+    it('should get data for one image', () => {
+      const imgDataResponse: ImageDataResponse = { imageId: 'id' }
+      apiServiceSpy.getImageById.and.returnValue(of(imgDataResponse))
+      component.imageData = imageData
 
-    expect(msgServiceSpy.error).toHaveBeenCalledWith({ summaryKey: 'GENERAL.IMAGES.NOT_FOUND' })
+      component.fetchImageData()
+
+      expect(component.images).toContain(imgDataResponse)
+    })
+
+    it('should handle error when fetching imageData', () => {
+      apiServiceSpy.getImageById.and.returnValue(throwError(() => new Error()))
+      component.imageData = [{ id: '123', imageId: '123', visible: true, position: '1', workspaceName: 'w1' }]
+
+      component.fetchImageData()
+
+      expect(msgServiceSpy.error).toHaveBeenCalledWith({ summaryKey: 'GENERAL.IMAGES.NOT_FOUND' })
+    })
   })
 
   describe('buildImageSrc', () => {
-    it('should return base64 data URL if image is found', () => {
+    it('should return base64 string if image is found', () => {
       component.images = [{ imageId: '123', mimeType: 'image/png', imageData: new Blob() }]
 
       const result = component.buildImageSrc(imageData[0])
