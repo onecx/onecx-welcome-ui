6e42ced4ddfddad5229489bd62a18adb78f52b20
Henry Taeschner <github@henry-taeschner.de>
2025-01-28 17:33:50 +0100

    feat: update openapi

--- src/app/shared/generated/api/api.ts
+++ src/app/shared/generated/api/api.ts
@@ -1,5 +1,3 @@
-export * from './announcements.service';
-import { AnnouncementsAPIService } from './announcements.service';
 export * from './imagesInternal.service';
 import { ImagesInternalAPIService } from './imagesInternal.service';
-export const APIS = [AnnouncementsAPIService, ImagesInternalAPIService];
+export const APIS = [ImagesInternalAPIService];
